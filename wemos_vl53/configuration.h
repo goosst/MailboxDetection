4b16cd8c908f050799e495169dfbd93e57379971
stijn <goosst@gmail.com>
2020-07-26 14:52:02 +0200

    added generic configuration.h

---
+++ wemos_vl53/configuration.h
@@ -0,0 +1,8 @@
+//WIFI configuration
+#define wifi_ssid "wifiname"
+#define wifi_password "wifipassword"
+
+//MQTT configuration
+#define mqtt_server "192.168.0.xxx"
+#define mqtt_user "mqtt_user"
+#define mqtt_password "mqtt_password"
